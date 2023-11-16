c25ad53810fad44ca6e20d7bcb7bba610ec29a85
Akash kumar Nayak <akashnayak3009@gmail.com>
2023-11-17 01:52:44 +0530

    api

--- api/users/usersControllers.js
+++ api/users/usersControllers.js
@@ -1,13 +1,63 @@
-import User from "./usersModel.js"
+import User from "./usersModel.js";
 
+export const createUser = async (req, res) => {
+    const { firstName, lastName, email } = req.body;
+    try {
+        const newUser = await User.create({ firstName, lastName, email });
+        res.status(201).json(newUser);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+};
 
-export const getAllUsers = async(req,res)=>{
-    try{
+export const getAllUsers = async (req, res) => {
+    try {
         const users = await User.findAll();
-        res.json({message:"All are fetched"});
-    }
-    catch(error){
+        res.status(200).json({ status: 1, message: "All are fetched" });
+    } catch (error) {
         console.log(error);
-        res.status(500).json({message:"Getting all users failed"})
+        res.status(500).json({ status: 0, message: "Getting all users failed" });
     }
-}
+};
+
+export const getUserById = async (req, res) => {
+    const { userId } = req.params;
+    try {
+        const user = await User.findByPk(userId);
+        if (!user) {
+            return res.status(404).json({ message: "User not found", user });
+        }
+        return res.status(200).json({ status: 1, message: "All are fetched" });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ status: 0, message: "Getting user failed" });
+    }
+};
+
+export const updateUser = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const user = await User.findByPk(id);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        await user.update(req.body, { new: true });
+        res.json(user);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+};
+
+export const deleteUser = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const user = await User.findByPk(id);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        await user.destroy();
+        res.json({ message: "User deleted successfully" });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
--- api/users/usersRoute.js
+++ api/users/usersRoute.js
@@ -1,8 +1,27 @@
-import express from 'express';
-import { getAllUsers } from './usersControllers.js';
+import express from "express";
+import {
+    createUser,
+    deleteUser,
+    getAllUsers,
+    getUserById,
+    updateUser,
+} from "./usersControllers.js";
 
 const router = express.Router();
 
-router.get('/', getAllUsers);
+// GET all users
+router.get("/", getAllUsers);
+
+// GET user by ID
+router.get("/:id", getUserById);
+
+// CREATE a new user
+router.post("/", createUser);
+
+// UPDATE user by ID
+router.put("/:id", updateUser);
+
+// DELETE user by ID
+router.delete("/:id", deleteUser);
 
 export default router;
